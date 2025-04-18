64e595f5df4e2c59826fafda1cafa57a55eccc14
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-04-18 23:38:26 +0300

    remove version

--- main.tf
+++ main.tf
@@ -4,11 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.88.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
     }
   }
 }
