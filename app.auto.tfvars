ea25a1ff8941f55ec8fa54d647d8df45c6a2ec2d
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-06-01 22:36:34 +0300

    improve config

--- app.auto.tfvars
+++ app.auto.tfvars
@@ -16,33 +16,14 @@ projects = [
   "store-pod/merchant-ui",
   "store-pod/store-pod-saas-gateway"
 ]
-cvhome-config = {
-  "trackUsage" : "false",
-  "usageExecededAction" : "continue",
-  "nonRenewedSubscriptionAction" : "continue"
-}
-smtp-config = {
-  "host" : "",
-  "username" : "",
-  "password" : "",
-  "port" : ""
+domain-config = {
+  domain : ""
 }
 stripe-config = {
   "stripeKey" : "",
   "stripeWebhockSigningKey" : ""
 }
-pods-config = [
-  {
-    index : 0
-    id : "1"
-    name : "default",
-    size : "large"
-  },
-  # {
-  #   index : 1
-  #   id : "2"
-  #   name : "o-21f023932bc66470c104b76f",
-  #   org : "21f023932bc66470c104b76f"
-  #   size : "x-large",
-  # }
-]
+kc-config = {
+  username : "sys-admin@mail.com",
+  password : ""
+}
--- config.tf
+++ config.tf
@@ -13,31 +13,13 @@ resource "aws_ssm_parameter" "config-stripe" {
 resource "aws_ssm_parameter" "config-domain" {
   name = "/${var.project}/config/domain"
   type = "String"
-  value = jsonencode({
-    "domain" : ""
-  })
-}
-resource "aws_ssm_parameter" "config-smtp" {
-  name = "/${var.project}/config/smtp"
-  type = "String"
-  value = jsonencode(var.smtp-config)
+  value = jsonencode(var.domain-config)
 }
 resource "aws_ssm_parameter" "config-kc" {
   name = "/${var.project}/config/kc"
   type = "String"
-  value = jsonencode({
-    "username" : "sys-admin@mail.com",
-    "password" : random_password.password.result
+  value = merge(jsonencode(var.kc-config), {
+    password : random_password.password.result
   })
-}
-resource "aws_ssm_parameter" "config-pods" {
-  name = "/${var.project}/config/pods"
-  type = "String"
-  value = jsonencode(var.pods-config)
-}
-resource "aws_ssm_parameter" "config-cvhome" {
-  name = "/${var.project}/config/cvhome"
-  type = "String"
-  value = jsonencode(var.cvhome-config)
 }
 
